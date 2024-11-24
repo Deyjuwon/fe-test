c6d379d0736997aaf45e859015d42439e352e0c2
Deyjuwon <temitayoadejuwon@gmail.com>
2024-11-24 19:02:52 +0100

    dashboard header done

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.scss';
 import { Route, Routes } from 'react-router-dom';
 import Login from "./pages/Login/Login"
-
+import Dashboard from './pages/Dashboard/Dashboard';
 
 function App() {
   return (
     <div className="">
       <Routes>
         <Route path='/login' element={<Login />} />
+        <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
     </div>
   );
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import logo from '../../assets/logo.svg';
+import classes from '../Header/Header.module.scss'
+import { CiSearch } from "react-icons/ci";
+import bell from '../../assets/np_notification_2425223_000000 1.png'
+import profile from '../../assets/avatar.png'
+import dropdown from '../../assets/np_dropdown_615120_000000 1.png'
+const Header = () => {
+  return (
+    <header className={classes.header}>
+    
+    {/* lhs */}
+      <div className={classes.leftHeader}>
+        <img src={logo} alt="" />
+        <div className={classes.headerInput}>
+            <input placeholder='Search for anything' type="text" />
+            <span>
+                <CiSearch size={20} color='white' />
+            </span>
+        </div>
+      </div>
+
+      {/* rhs */}
+      <div className={classes.rightHeader}>
+        <a href="#docs">Docs</a>
+        <img src={bell} alt="" />
+        <div className={classes.profileImg}>    
+            <img src={profile} alt="" />
+            <p>Adedeji</p>
+            <img src={dropdown} alt="" />
+        </div>
+        
+
+      </div>
+    </header>
+  )
+}
+
+export default Header
