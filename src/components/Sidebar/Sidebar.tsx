56e64cec10b0a3a491ba4ca30ec8c3e0ecdade05
Deyjuwon <temitayoadejuwon@gmail.com>
2024-12-09 21:34:12 +0100

    Fix: Apply active class only to the Users menu item in the Sidebar component

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -4,15 +4,16 @@ import { MenuSection } from '../../pages/Dashboard/types';
 import { menuData, customersData, businessData, settingsData } from '../../pages/Dashboard/menuData';
 import classes from './Sidebar.module.scss';
 
-interface SidebarProps {}
-
 const Sidebar: React.FC = () => {
   // Reusable function to render a menu section
   const renderMenuSection = (title: string | null, items: MenuSection[]) => (
     <div>
       {title && <p className={classes.asideHeader}>{title}</p>}
       {items.map((item, index) => (
-        <span key={index} className={classes.menuItem}>
+        <span
+          key={index}
+          className={item.label === 'Users' ? classes.active : ''}
+        >
           <img src={item.icon} alt={item.label} />
           <p>{item.label}</p>
           {item.hasDropdown && <img src={drop} alt="dropdown-icon" />}
